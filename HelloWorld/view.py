4d1a2d56f265b22f54493806af0dce6b59ad0efe
成焱 <chengyan5535@dingtalk.com>
2019-11-17 22:37:24 +0800

    【测试】10086

--- HelloWorld/view.py
+++ HelloWorld/view.py
@@ -33,7 +33,7 @@ def hello(request):
     os.system('nginx -s reload')
     return HttpResponse('success')
 def testGouzi(request):
-    return HttpResponse('999')
+    return HttpResponse('10086')
 
 class DateEncoder(json.JSONEncoder):
     def default(self, obj):
