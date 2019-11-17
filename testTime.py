145b3382f7d1da63bff52f07414756116719678f
成焱 <chengyan5535@dingtalk.com>
2019-11-17 20:56:32 +0800

    【功能】测试一个定时脚本，让服务端定时更新

---
+++ testTime.py
@@ -0,0 +1,14 @@
+#-*-coding:utf-8-*- 
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: 成焱
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: ab364743113@126.com
+@file: testTime.py
+@time: 2019/11/17 8:55 PM
+@desc:
+'''
+import os
+import datetime
+print(datetime.datetime.now())
