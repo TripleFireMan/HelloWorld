be68a6ae40d57b215d3f6f6bfcce6bce5287b2d8
成焱 <ab364743113@126.com>
2021-11-04 16:03:56 +0800

    【功能】修改全部性别下不展示道具的问题

--- ZhuaZhou/views.py
+++ ZhuaZhou/views.py
@@ -12,6 +12,9 @@ from rest_framework.decorators import api_view
 from easy_thumbnails.options import ThumbnailOptions
 from HelloWorld.settings import MEDIA_URL
 from filer.storage import PublicFileSystemStorage
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_apscheduler.jobstores import DjangoJobStore, register_events, register_job
+
 class DateEncoders(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
@@ -27,7 +30,9 @@ def tools(request):
     print(versions)
     result = Result()
     gender = request.GET.get('gender')
-    if  gender == '男':
+    if  gender == None:
+          pass
+    elif  gender == '男':
         versions = versions.exclude(gender = '女')
     else:
         versions = versions.exclude(gender = '男')
@@ -51,3 +56,24 @@ def tools(request):
         arr.append(r_dic)
     dic['data'] = arr
     return HttpResponse(json.dumps(dic, cls=DateEncoders))
+
+# #开启定时工作
+#
+# try:
+#     # 实例化调度器
+#     scheduler = BackgroundScheduler()
+#     # 调度器使用DjangoJobStore()
+#     scheduler.add_jobstore(DjangoJobStore(), "default")
+#     # 设置定时任务，选择方式为interval，时间间隔为10s
+#     # 另一种方式为每天固定时间执行任务，对应代码为：
+#     # @register_job(scheduler, 'cron', day_of_week='mon-fri', hour='9', minute='30', second='10',id='task_time')
+#     @register_job(scheduler,"interval", seconds=10)
+#     def my_job1():
+#         # 这里写你要执行的任务
+#         print('250')
+#     register_events(scheduler)
+#     scheduler.start()
+# except Exception as e:
+#     print(e)
+#     # 有错误就停止定时器
+#     scheduler.shutdown()
