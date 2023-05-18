7acc21fb16c5e41f2cc262d2b8876531f3217a44
成焱 <ab364743113@126.com>
2023-05-18 15:43:12 +0800

    【功能】样式修改

--- TYMetro/admin.py
+++ TYMetro/admin.py
@@ -5,8 +5,13 @@ from TYMetro.models import FeedBack
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('nick_name',"birthday","gender","address","mobile","introduce","email","img","third_source","apple_id")
-
+    exclude = ('date_joined','first_name','last_name','is_staff','is_active','last_login','is_superuser','groups','user_permissions')
+    list_display = ('username','nick_name',"birthday","gender","address","mobile","introduce","email","img","third_source","apple_id")
+    fieldsets = (
+        ('基本信息',{'fields':('username',('nick_name','birthday',"gender",),'introduce','img',"address",)}),
+        ('联系方式',{'fields':("mobile",'email')}),
+        ('其他',{'fields':("third_source","apple_id")}),
+    )
 
 admin.site.register(UserProfile,UserProfileAdmin)
 admin.site.register(FeedBack)
