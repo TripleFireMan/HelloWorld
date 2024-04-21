eea6c5bdca4501c0831c9606cfe59e0ab458568a
成焱 <ab364743113@126.com>
2024-04-21 17:59:59 +0800

    update

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -101,7 +101,6 @@ urlpatterns = [
     url(r'^files/', include('filer.urls')),
     path('api/', include(router.urls)),
     path('gitPull',view.gitPull),
-
     url(r'^api-auth/',include('rest_framework.urls')),
     # url(r'^static/(?P<path>.*)$', static.serve,{'document_root': settings.STATIC_ROOT}, name='static'),
 ]
